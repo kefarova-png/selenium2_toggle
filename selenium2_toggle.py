eb5a67d66810d8048f6ca9364b5e2ba2d27cc6d5
kefarova-png <kefarova@gmail.com>
2026-06-17 19:59:59 +0400

    Version 2.1. Добавлен количественный контроль смещения слайдера

--- selenium2_toggle.py
+++ selenium2_toggle.py
@@ -1,9 +1,9 @@
 #  Импортируем необходимые библиотеки и модули
 import time
-from selenium.webdriver import ActionChains
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 
 #  Chrome. Создаём переменную для опций браузера
@@ -18,29 +18,42 @@ driver = webdriver. Chrome (
 base_url = 'https://the-internet.herokuapp.com/horizontal_slider'
 driver.get(base_url)
 driver.set_window_size(920, 1080)
-time.sleep(2)
-print('_'*20)
+time.sleep(1)
 
-#  Создаём экземпляр класса ActionChains для перемещения по окну браузера
-actions = ActionChains(driver)
+#  Проверка для случая, когда изначально слайдер стоит в крайнем левом (нулевом) положении, смещение на 1 шаг меняет отображаемое значение слайдера на 0.5 единиц
 
-#  Найдём слайдер и найдём-запомним его значение
+#  Найдём слайдер и запомним его отображаемое числовое значение
 slider = driver.find_element(By.XPATH, "//input[@type='range']")
-time.sleep(3)
-slider_value_element = driver.find_element(By.ID, "range")
-initial_slider_value = slider_value_element.text
+time.sleep(1)
+initial_slider_value = driver.find_element(By.ID, "range").text
 
-#  Передвинем слайдер вправо на 50 пикселей
-actions.click_and_hold(slider).move_by_offset(50,0).release().perform()
+step_number = 0  #  Счётчик шагов проверки
+print('_'*21,'\nStep\t|Expect\t|Fact',sep='')  #  Заголовок таблицы
+
+#  Передвинем слайдер вправо 10 раз на 1 шаг и проконтролируем смещение
+for i in range(10):
+    slider.send_keys(Keys.ARROW_RIGHT)
+    time.sleep(0.5)
+    step_number += 1
+    expected_shift = (0 + 0.5 * (i + 1))
+    fact_position = driver.find_element(By.ID, "range").text
+    fact_shift = float(fact_position) - float(initial_slider_value)
+    print(step_number, '\t'*2, expected_shift, '\t', fact_shift)
+    assert fact_shift == float(expected_shift), 'Смещение не соответствует ожидаемому'
+
+#  Передвинем слайдер влево 10 раз на 1 шаг и проконтролируем смещение
+for i in range(10):
+    slider.send_keys(Keys.ARROW_LEFT)
+    time.sleep(0.5)
+    step_number += 1
+    expected_shift = (5 - 0.5 * (i + 1))
+    fact_position = driver.find_element(By.ID, "range").text
+    fact_shift = float(fact_position) - float(initial_slider_value)
+    print(step_number, '\t'*2, expected_shift, '\t', fact_shift)
+    assert fact_shift == float(expected_shift), 'Смещение не соответствует ожидаемому'
+
+time.sleep(1)
+print('_'*20,'\nСмещения слайдера внутри диапазона перемещения корректны',sep='')
+
 time.sleep(2)
-print("Слайдер передвинут в сторону увеличения")
-
-#  Проверим, что значение слайдера изменилось
-final_slider_value = slider_value_element.text
-delta = float(final_slider_value) - float(initial_slider_value)
-assert delta > 0, 'Значение слайдера должно увеличиться.'
-print(f"Значение слайдера увеличилось на {delta}")
-
-time.sleep(3)
-#  Закрываем браузер
-driver.close()
+driver.close()  #  Закрываем браузер
