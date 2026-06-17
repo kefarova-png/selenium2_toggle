c2028fcab9d5befa19e4c20c39acda73a21974c7
kefarova-png <kefarova@gmail.com>
2026-06-18 01:24:12 +0400

    Version 3. Добавлен параметр движения, позволяющий упростить программу код

--- selenium2_toggle.py
+++ selenium2_toggle.py
@@ -1,3 +1,5 @@
+#  Проверка для случая, когда изначально ползунок слайдер стоит в крайнем левом (нулевом) положении, смещение ползунка слайдера на 1 шаг соответствует изменению отображаемого значения слайдера на 0.5 единиц.
+
 #  Импортируем необходимые библиотеки и модули
 import time
 from selenium import webdriver
@@ -5,6 +7,19 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
+
+
+expected_shift = 0  #  Начальное ожидаемое смещение (нулевое)
+
+
+def motion_control():  #  Функция сравнения ожидаемого и фактического смещения
+    time.sleep(0.2)
+    global expected_shift
+    expected_shift = expected_shift + direction_sign  #  ожидаем, что смещение на 1 шаг меняет значение слайдера на ±0.5 единиц за шаг
+    fact_position = driver.find_element(By.ID, "range").text  #  фактическое значение слайдера
+    print(expected_shift, '\t', fact_position)
+    assert float(fact_position) == expected_shift, 'Смещение не соответствует ожидаемому'
+
 
 #  Chrome. Создаём переменную для опций браузера
 options = webdriver.ChromeOptions()
@@ -20,40 +35,25 @@ driver.get(base_url)
 driver.set_window_size(920, 1080)
 time.sleep(1)
 
-#  Проверка для случая, когда изначально слайдер стоит в крайнем левом (нулевом) положении, смещение на 1 шаг меняет отображаемое значение слайдера на 0.5 единиц
+print('_'*13,'\nExpect\t|Fact',sep='')  #  Заголовок таблицы
 
-#  Найдём слайдер и запомним его отображаемое числовое значение
+#  Найдём слайдер
 slider = driver.find_element(By.XPATH, "//input[@type='range']")
-time.sleep(1)
-initial_slider_value = driver.find_element(By.ID, "range").text
-
-step_number = 0  #  Счётчик шагов проверки
-print('_'*21,'\nStep\t|Expect\t|Fact',sep='')  #  Заголовок таблицы
 
 #  Передвинем слайдер вправо 10 раз на 1 шаг и проконтролируем смещение
-for i in range(10):
+direction_sign = 0.5  #  Параметр движения вправо, для изменения значения слайдера на 0.5 единиц/шаг
+for _ in range(10):
     slider.send_keys(Keys.ARROW_RIGHT)
-    time.sleep(0.5)
-    step_number += 1
-    expected_shift = (0 + 0.5 * (i + 1))
-    fact_position = driver.find_element(By.ID, "range").text
-    fact_shift = float(fact_position) - float(initial_slider_value)
-    print(step_number, '\t'*2, expected_shift, '\t', fact_shift)
-    assert fact_shift == float(expected_shift), 'Смещение не соответствует ожидаемому'
+    motion_control()
 
 #  Передвинем слайдер влево 10 раз на 1 шаг и проконтролируем смещение
-for i in range(10):
+direction_sign = -0.5  #  Параметр движения влево, для изменения значения слайдера на -0.5 единиц/шаг
+for _ in range(10):
     slider.send_keys(Keys.ARROW_LEFT)
-    time.sleep(0.5)
-    step_number += 1
-    expected_shift = (5 - 0.5 * (i + 1))
-    fact_position = driver.find_element(By.ID, "range").text
-    fact_shift = float(fact_position) - float(initial_slider_value)
-    print(step_number, '\t'*2, expected_shift, '\t', fact_shift)
-    assert fact_shift == float(expected_shift), 'Смещение не соответствует ожидаемому'
+    motion_control()
 
 time.sleep(1)
-print('_'*20,'\nСмещения слайдера внутри диапазона перемещения корректны',sep='')
+print('_'*13,'\nСмещения ползунка слайдера внутри диапазона перемещения корректны',sep='')
 
 time.sleep(2)
 driver.close()  #  Закрываем браузер
